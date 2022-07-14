2c8a90fb5922585740a091226feebf65dda55af1
SeraBocca <l5dpgkw9@alanwrist.anonaddy.com>
2022-07-14 12:14:01 +0200

    add tailwindcss

---
+++ src/.vuepress/theme/components/SidebarButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="cursor-pointer block lg:hidden w-5 h-5 absolute p-2.5 t-2.5 l-4"
+    @click="$emit('toggle-sidebar')"
+  >
+    <svg
+      class="block w-5 h-5"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      viewBox="0 0 448 512"
+    >
+      <path
+        fill="currentColor"
+        d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"
+        class=""
+      />
+    </svg>
+  </div>
+</template>
+
+<style lang="stylus">
+@import "../../styles/theme.styl";
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/.vuepress/**/*.{js,md,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        levelOne: "#011426",
+        levelTwo: "#031C34",
+        levelThree: "#0E3A64",
+        bodyText: "#C1DDF8",
+        links: "#22C55E",
+        accent: "#FAF0E6",
+        white: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
