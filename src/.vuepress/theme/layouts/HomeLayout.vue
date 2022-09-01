392958d85d18438b7bb334e43ebdf0f7ebcf7c1d
SeraBocca <l5dpgkw9@alanwrist.anonaddy.com>
2022-09-01 17:58:17 +0300

    adjust top section layout

--- src/.vuepress/theme/layouts/HomeLayout.vue
+++ src/.vuepress/theme/layouts/HomeLayout.vue
@@ -1,18 +1,18 @@
 <template>
   <div
-    class="relative w-screen min-h-screen overflow-hidden flex flex-col items-center bg-levelOne"
+    class="flex overflow-hidden relative flex-col items-center w-screen min-h-screen bg-levelOne"
     @scroll="handleScroll"
   >
     <div
-      class="relative h-screen w-full bg-cover bg-no-repeat flex flex-col items-center justify-between"
+      class="flex relative flex-col justify-center items-center w-full h-screen bg-no-repeat bg-cover"
       :style="{
         backgroundImage: `radial-gradient(#01142600 0%,#011426 100%), url(${data.background})`,
       }"
     >
-      <nav class="md:flex justify-end mt-8 w-full" v-if="data.links">
+      <nav class="absolute top-0 right-0 justify-end mt-8 w-full md:flex" v-if="data.links">
         <div class="flex flex-wrap justify-around md:block md:mr-8 lg:mr-16">
           <div
-            class="md:flex justify-end align-center mb-4 lg:mb-8 md:text-right md:ml-auto"
+            class="justify-end mb-4 md:flex align-center lg:mb-8 md:text-right md:ml-auto"
             v-for="link of data.links"
             :key="link.url"
           >
@@ -26,67 +26,13 @@
 
       <div class="flex flex-col items-center w-screen">
         <img
-          class="relative w-11/12 md:w-[600px] m-auto"
+          class="relative w-11/12 md:w-[220px] m-auto"
           :src="data.heroImage"
         />
-        <div class="w-max-content text-center text-lg lg:text-2xl">
+        <div class="mt-8 text-lg text-center w-[680px] lg:text-2xl ">
           {{ data.tagline }}
         </div>
-
-        <div class="mt-14 flex flex-col items-center text-lg lg:text-2xl">
-          <div class="w-max-content">{{ data.actionText }}</div>
-          <div class="mt-6 w-full md:flex flex-rol flex-wrap justify-center">
-            <a
-              v-for="feature of data.features"
-              :key="feature.value"
-              :href="`#${feature.value}`"
-              @click="scrollTo($event, feature.value)"
-            >
-              <button
-                class="relative z-[30] w-44 lg:w-56 mb-6 rounded-lg text-sm lg:text-lg mx-3 py-2 lg:px-8 lg:py-4 bg-transparent cursor-pointer text-white border-solid border-[1.5px] border-white hover:border-links hover:text-links font-bold block md:inline"
-              >
-                {{ feature.title }}
-              </button>
-            </a>
-          </div>
-        </div>
-
-        <div
-          class="mt-28 mx-4 text-center text-base lg:text-2xl relative z-[15]"
-        >
-          Or check out our
-          <a
-            class="relative underline cursor-pointer"
-            href="https://github.com/stratum-mining"
-            target="_blank"
-            rel="noopener noreferrer"
-            >documentation</a
-          >
-          and
-          <a
-            class="underline cursor-pointer"
-            href="https://github.com/stratum-mining"
-            target="_blank"
-            rel="noopener noreferrer"
-            >contribute</a
-          >
-        </div>
       </div>
-
-      <div
-        class="text-center mb-6 text-bodyText transition-opacity z-[30]"
-        v-bind:style="{ opacity: showGradientBg ? '0' : '1' }"
-      >
-        {{ data.footer }}
-      </div>
-
-      <div
-        class="absolute z-[10] left-0 -bottom-36 h-96 w-full transition-opacity"
-        v-bind:style="{
-          opacity: showGradientBg ? '1' : '0',
-          background: 'linear-gradient(#01142600 0%, #011426 60%)',
-        }"
-      ></div>
     </div>
 
     <div class="flex flex-col pt-36 mb-10 z-[20]">
@@ -132,7 +78,7 @@
         :id="feature.value"
       >
         <div class="flex flex-col w-[460px] items-start mx-auto mb-8 md:mb-12">
-          <h2 class="border-b-none mb-6 mt-0">{{ feature.title }}</h2>
+          <h2 class="mt-0 mb-6 border-b-none">{{ feature.title }}</h2>
           <p class="m-0 text-left">{{ feature.text }}</p>
         </div>
         <div
