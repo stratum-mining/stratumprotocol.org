88596df4c7c9281657f29135dfe4c818b94f9e19
plebhash <plebhash@proton.me>
2024-04-29 16:53:55 -0500

    finish shell.nix

--- shell.nix
+++ shell.nix
@@ -18,5 +18,24 @@ pkgs.mkShell {
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+    # install nodejs (16 to avoid errors in the next step)
+    nvm install 16
+
+    # install yarn
+    npm install -g yarn
+
+    # update yarn
+    npm update
+
+    printf '%s\n' \
+'=========================================' \
+'Now you have a Nix environment with NodeJS v16 plus Yarn.' \
+'You can run the following for your development setup:' \
+'$ # Install dependencies' \
+'$ yarn install' \
+'$ # Serve locally (by default on port 8080)' \
+'$ yarn dev'
+
   '';
 }
