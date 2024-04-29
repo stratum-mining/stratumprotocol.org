7858407b598f88bee9157ad7ffb805e990995faa
plebhash <plebhash@proton.me>
2024-04-29 15:26:20 -0500

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -10,14 +10,13 @@ pkgs.mkShell {
     pkgs.git
   ];
 
+  # Install nvm
   shellHook = ''
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+    # Load nvm into shell environment
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  '';
-
-  # Install nvm
-  postShellHooks = ''
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${nvmVersion}/install.sh | bash
   '';
 }
